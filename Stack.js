4790c5dfc522edcaa9ff911c297c0b88c53e13f3
kadonomaro <denis.afer@gmail.com>
2020-06-21 18:53:57 +0700

    feat: update stack structure

--- Stack.js
+++ Stack.js
@@ -1,27 +1,38 @@
 class Stack {
     constructor() {
         this.elements = [];
+        this.length = 0;
     }
 
     push(element) {
-        this.elements.push(element);
+        if (element) {
+            this.elements.push(element);
+            this.length++;
+        } else {
+            throw new Error('Element must not be empty')
+        }
     }
 
     pop() {
-        return this.elements.pop();
+        if (this.length > 0) {
+            this.length--;
+            return this.elements.pop();
+        }
+        return
     }
 
     peek() {
-        return this.elements[this.elements.length - 1]
+        return this.elements[this.length - 1];
     }
 
     size() {
-        return this.elements.length
+        return this.length;
     }
 }
 
 const stack = new Stack();
 
+stack.pop();
 stack.push(1);
 stack.push(2);
 stack.push(3);
